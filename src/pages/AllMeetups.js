c2ee9ec4dcd6958b5c6b88722a408da85e131874
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2022-01-16 14:33:35 -0600

    Added Items in Meetup Items

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,31 @@
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image: 'https://imgur.com/a/XUWxAAR',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description: 'This is a first amazing meetup',
+    },
+    {
+        id: 'm2',
+        title: 'This is a Second meetup',
+        image: 'https://imgur.com/a/XUWxAAR',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description: 'This is a first amazing meetup',
+    }
+]
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  return (
+      <section>
+          <h1>All Meetups</h1>
+          <ul>
+              {DUMMY_DATA.map((meetup) => {
+                  return <li key={meetup.id}>{meetup.title}</li>;
+              })}
+          </ul>
+      </section>
+  )
 }
 
 export default AllMeetupsPage;
